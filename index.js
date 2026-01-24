e84b14be24ce1e1d841724c30ea94a3c9f3fae9d
alsabithk3333-ai <alsabithk3333@gmail.com>
2026-01-24 12:21:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function startBot () {
   const bot = mineflayer.createBot({
     host: 'Nii111.aternos.me',
     port: 34596,
-    username: 'AFK_Bot',
+    username: 'local',
     version: '1.21.3'
   })
 
